48fb7b703ea6bbc634dd5f12091e51c11382481e
huutien3946 <huutien3946@gmail.com>
2022-06-06 20:59:15 +0700

    lam toi trang 44

--- TrinhHuuTien_BigSchool/Controllers/AttendancesController.cs
+++ TrinhHuuTien_BigSchool/Controllers/AttendancesController.cs
@@ -5,10 +5,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TrinhHuuTien_BigSchool.DTOs;
 using TrinhHuuTien_BigSchool.Models;
 
 namespace TrinhHuuTien_BigSchool.Controllers
 {
+    [Authorize]
     public class AttendancesController : ApiController
     {
         private ApplicationDbContext _dbContext;
@@ -17,12 +19,28 @@ namespace TrinhHuuTien_BigSchool.Controllers
             _dbContext = new ApplicationDbContext();
         }
 
-        [HttpPost]
+        /*[HttpPost]
         public IHttpActionResult Attend([FromBody] int courseId)
         {
             var attendance = new Attendance
             {
                 CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }*/
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+
+            var attendance = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
                 AttendeeId = User.Identity.GetUserId()
             };
             _dbContext.Attendances.Add(attendance);
---
+++ TrinhHuuTien_BigSchool/DTOs/AttendanceDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrinhHuuTien_BigSchool.DTOs
+{
+    public class AttendanceDto
+    {
+        public int CourseId { get; set; }
+    }
+}
